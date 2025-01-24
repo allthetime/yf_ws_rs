ad54180f3f5f8f08e0df9451c0fd8f4d28591f89
allthetime <chronic88@gmail.com>
2025-01-24 14:00:01 -0800

    socket connects, sends, receives. prost decode failing currently

--- build.rs
+++ build.rs
@@ -1,14 +1,5 @@
-use std::env;
 use std::io::Result;
-use std::path::PathBuf;
-
 fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    prost_build.file_descriptor_set_path(
-        PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
-            .join("file_descriptor_set.bin"),
-    );
-
-    prost_build.compile_protos(&["src/yaticker.proto"], &["src/"])?;
+    prost_build::compile_protos(&["src/yaticker.proto"], &["src/"])?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::env;
-use std::path::PathBuf;
-
 // use openssl::ssl::SslStream;
 use prost::Message as ProstMessage;
 use serde_json::{json, Map, Value};
 use tungstenite::http::{response, Uri};
 use tungstenite::{connect, Message};
-
-use prost_reflect::{DescriptorPool, DynamicMessage, Value as ProstValue};
 
 const YAHOO_FINANCE_WEBSOCKET_URL: &str = "wss://streamer.finance.yahoo.com/";
 const TICKER_SYMBOLS: [&str; 1] = ["AAPL"];
@@ -41,43 +36,20 @@ fn main() {
 
     socket.send(msg).unwrap();
 
-    let file_descriptor_path =
-        PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
-            .join("file_descriptor_set.bin");
-
-    let file_descriptor_bytes =
-        std::fs::read(file_descriptor_path).expect("Failed to read file descriptor");
-    let pool = DescriptorPool::decode(file_descriptor_bytes.as_ref()).unwrap();
-    let message_descriptor = pool.get_message_by_name("yaticker.yaticker").unwrap();
-
     loop {
         let msg = socket.read().unwrap();
         match msg {
             Message::Text(txt) => {
                 println!("Received TXT: {}", txt);
-
-                let dynamic_message =
-                    DynamicMessage::decode(message_descriptor.clone(), txt.as_bytes());
-
-                match dynamic_message {
-                    Ok(dynamic_message) => {
-                        println!("Received DynamicMessage: {:?}", dynamic_message);
+                match Yaticker::decode(txt.as_bytes()) {
+                    Ok(yaticker) => {
+                        println!("Received Yaticker: {:?}", yaticker);
                     }
                     Err(e) => {
-                        print!("Error decoding DynamicMessage: ");
+                        print!("Error decoding Yaticker: ");
                         dbg!(e);
                     }
                 }
-
-                // match Yaticker::decode(txt.as_bytes()) {
-                //     Ok(yaticker) => {
-                //         println!("Received Yaticker: {:?}", yaticker);
-                //     }
-                //     Err(e) => {
-                //         print!("Error decoding Yaticker: ");
-                //         dbg!(e);
-                //     }
-                // }
             }
             Message::Binary(bin) => {
                 println!("Received BIN: {:?}", bin);
