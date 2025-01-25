9dba74d5114793155e04d0cf3f0176540b673104
allthetime <chronic88@gmail.com>
2025-01-24 16:36:28 -0800

    some abstraction - think about design

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,64 @@
-// use openssl::ssl::SslStream;
+use base64::prelude::*;
 use prost::Message as ProstMessage;
 use serde_json::{json, Map, Value};
 use tungstenite::http::Uri;
 use tungstenite::{connect, Message};
 
-use base64::prelude::*;
-
-const YAHOO_FINANCE_WEBSOCKET_URL: &str = "wss://streamer.finance.yahoo.com/";
-const TICKER_SYMBOLS: [&str; 1] = ["AAPL"];
-
 pub mod yaticker {
     include!(concat!(env!("OUT_DIR"), "/yaticker.rs"));
 }
-
 use yaticker::Yaticker;
 
-fn build_subscription_object(ticker_symbols: Vec<&str>) -> Value {
+const YAHOO_FINANCE_WEBSOCKET_URL: &str = "wss://streamer.finance.yahoo.com/";
+const TICKER_SYMBOLS: [&str; 1] = ["BTC-USD"];
+
+struct Heart {
+    timestamp: std::time::Instant,
+}
+
+impl Heart {
+    fn new() -> Self {
+        Self {
+            timestamp: std::time::Instant::now(),
+        }
+    }
+    fn beat(&mut self, to_do: Box<dyn Fn(&std::time::Instant) -> std::time::Instant>) {
+        self.timestamp = (to_do)(&self.timestamp);
+    }
+}
+
+type TungsteniteSocket =
+    tungstenite::WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>;
+
+fn build_subscription_json() -> Value {
     let mut map = Map::new();
-    let ticker_symbols_json = json!(ticker_symbols);
+    let ticker_symbols_json = json!(TICKER_SYMBOLS);
     map.insert("subscribe".to_string(), ticker_symbols_json);
     Value::Object(map)
+}
+
+fn subscribe_to_tickers(socket: &mut TungsteniteSocket) {
+    let subscription_json = build_subscription_json();
+    let msg = Message::Text(subscription_json.to_string().into());
+    socket.send(msg).unwrap();
+}
+
+fn connect_socket(uri: Uri) -> TungsteniteSocket {
+    let (socket, _response) = connect(uri).expect("Connection Failure");
+    socket
 }
 
 fn main() {
     let uri: Uri = YAHOO_FINANCE_WEBSOCKET_URL.parse().unwrap();
 
-    let (mut socket, response) = connect(uri).expect("Failed to connect");
-    let subscription_json = build_subscription_object(TICKER_SYMBOLS.to_vec());
-    let msg = Message::Text(subscription_json.to_string().into());
+    let mut socket: TungsteniteSocket = connect_socket(uri);
 
-    socket.send(msg).unwrap();
+    subscribe_to_tickers(&mut socket);
+
+    let mut heart: Heart = Heart::new();
+
+    // poll socket
+    // TODO: make this async
 
     loop {
         let msg = socket.read().unwrap();
@@ -48,11 +77,32 @@ fn main() {
                     }
                 }
             }
-            Message::Binary(bin) => (),
-            Message::Ping(ping) => (),
-            Message::Pong(pong) => (),
-            Message::Close(close) => (),
-            Message::Frame(frame) => (),
+            Message::Binary(bin) => {
+                println!("Received Binary message: {:?}", bin);
+            }
+            Message::Ping(ping) => {
+                heart.beat(Box::new(|last_heartbeat| {
+                    let now = std::time::Instant::now();
+                    let time_since_last_heartbeat = now.duration_since(*last_heartbeat);
+                    dbg!(time_since_last_heartbeat);
+                    now
+                }));
+
+                /*
+                    Send Keep Alive message
+                */
+                let msg = Message::Pong(ping);
+                socket.send(msg).unwrap();
+            }
+            Message::Pong(pong) => {
+                println!("Received Pong message: {:?}", pong);
+            }
+            Message::Close(close) => {
+                println!("Received Close message: {:?}", close);
+            }
+            Message::Frame(frame) => {
+                println!("Received Frame message: {:?}", frame);
+            }
         }
     }
 }
