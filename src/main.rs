0379c65aaedaefcb3531807f7152a527dbfe65c1
Christopher Reid <chronic88@gmail.com>
2025-02-05 07:46:36 -0800

    push

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::io::Read;
-
 use base64::prelude::*;
+use chrono::DateTime;
+use chrono_tz::US;
 use prost::Message as ProstMessage;
 use serde_json::{json, Map, Value};
-use streamz::streamer;
-use tungstenite::http::Uri;
-use tungstenite::{connect, Message};
+use streamz::streamer::{Streamer, TungsteniteSocket};
+use tungstenite::{connect, Message, Utf8Bytes};
 
 pub mod yaticker {
     include!(concat!(env!("OUT_DIR"), "/yaticker.rs"));
@@ -13,30 +12,12 @@ pub mod yaticker {
 use yaticker::Yaticker;
 
 const YAHOO_FINANCE_WEBSOCKET_URL: &str = "wss://streamer.finance.yahoo.com/";
-const TICKER_SYMBOLS: [&str; 1] = ["BTC-CAD"];
-
-struct Heart {
-    timestamp: std::time::Instant,
-}
-
-impl Heart {
-    fn new() -> Self {
-        Self {
-            timestamp: std::time::Instant::now(),
-        }
-    }
-    fn beat(&mut self, to_do: Box<dyn Fn(&std::time::Instant) -> std::time::Instant>) {
-        self.timestamp = (to_do)(&self.timestamp);
-    }
-}
-
-type TungsteniteSocket =
-    tungstenite::WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>;
-
+// const TICKER_SYMBOLS: [&str; 1] = ["HIVE.V"];
+const TICKER_SYMBOLS: [&str; 1] = ["BTC-USD"];
 
 fn build_subscription_json() -> Value {
-    let mut map = Map::new();
-    let ticker_symbols_json = json!(TICKER_SYMBOLS);
+    let mut map: Map<String, Value> = Map::new();
+    let ticker_symbols_json: Value = json!(TICKER_SYMBOLS);
     map.insert("subscribe".to_string(), ticker_symbols_json);
     Value::Object(map)
 }
@@ -47,73 +28,65 @@ fn subscribe_to_tickers(socket: &mut TungsteniteSocket) {
     socket.send(msg).unwrap();
 }
 
-fn connect_socket(uri: Uri) -> TungsteniteSocket {
+fn connect_socket(uri: &str) -> TungsteniteSocket {
     let (socket, response) = connect(uri).expect("Connection Failure");
-    print!("Connected to Yahoo Finance WebSocket: ");
     dbg!(response);
     socket
 }
 
-fn main() {
-    let uri: Uri = YAHOO_FINANCE_WEBSOCKET_URL.parse().unwrap();
-    let mut socket: TungsteniteSocket = connect_socket(uri);
-    subscribe_to_tickers(&mut socket);
-    let mut heart: Heart = Heart::new();
+fn get_datetime(timestamp_ms: i64, timezone: &chrono_tz::Tz) -> DateTime<chrono_tz::Tz> {
+    let timestamp: i64 = timestamp_ms / 1000;
+    let dt: DateTime<chrono::Utc> =
+        DateTime::from_timestamp(timestamp, 0).expect("Invalid timestamp");
+    dt.with_timezone(timezone)
+}
 
-    let streamer = streamer::Streamer {
-        stream: socket,
-        transformer: Box::new(|msg| {
-            print!("Received Message: ");
-            dbg!(msg);
-        }),
-    }
+fn print_format_datetime(dt: DateTime<chrono_tz::Tz>) -> String {
+    dt.format("%Y-%m-%d %H:%M:%S").to_string()
+}
 
-    // poll socket
-    // TODO: make this async
+fn get_decodable_btyes(txt: Utf8Bytes, buffer: &mut [u8; 2048]) -> usize {
+    let bytes = txt.as_bytes();
+    let decoded = BASE64_STANDARD.decode(bytes).unwrap();
+    buffer[..decoded.len()].copy_from_slice(&decoded);
+    decoded.len()
+}
 
-    loop {
-        let msg = socket.read().unwrap();
-
-        match msg {
-            Message::Text(txt) => {
-                let decoded = BASE64_STANDARD.decode(txt.as_bytes()).unwrap();
-                match Yaticker::decode(decoded.as_slice()) {
-                    Ok(yaticker) => {
-                        print!("Received Yaticker: ");
-                        dbg!(yaticker);
-                    }
-                    Err(e) => {
-                        print!("Error decoding Yaticker: ");
-                        dbg!(e);
-                    }
+fn handle_message(socket: &mut TungsteniteSocket, msg: Message, buffer: &mut [u8; 2048]) {
+    match msg {
+        Message::Text(txt) => {
+            let size = get_decodable_btyes(txt, buffer);
+            match Yaticker::decode(&buffer[..size]) {
+                Ok(yaticker) => {
+                    let timestamp_ms: i64 = yaticker.time;
+                    let dt = get_datetime(timestamp_ms, &US::Pacific);
+                    let formatted = print_format_datetime(dt);
+                    dbg!(formatted);
+                    dbg!(&yaticker);
+                }
+                Err(e) => {
+                    dbg!(e);
                 }
             }
-            Message::Binary(bin) => {
-                println!("Received Binary message: {:?}", bin);
-            }
-            Message::Ping(ping) => {
-                heart.beat(Box::new(|last_heartbeat| {
-                    let now = std::time::Instant::now();
-                    let time_since_last_heartbeat = now.duration_since(*last_heartbeat);
-                    dbg!(time_since_last_heartbeat);
-                    now
-                }));
-
-                /*
-                    Send Keep Alive message
-                */
-                let msg = Message::Pong(ping);
-                socket.send(msg).unwrap();
-            }
-            Message::Pong(pong) => {
-                println!("Received Pong message: {:?}", pong);
-            }
-            Message::Close(close) => {
-                println!("Received Close message: {:?}", close);
-            }
-            Message::Frame(frame) => {
-                println!("Received Frame message: {:?}", frame);
-            }
         }
+        Message::Ping(ping) => {
+            /*
+                Send Keep Alive message
+            */
+            let msg = Message::Pong(ping);
+            socket.send(msg).unwrap();
+        }
+        Message::Close(close) => {
+            dbg!(close);
+            std::process::exit(0);
+        }
+        _ => (),
     }
+}
+
+fn main() {
+    let mut socket = connect_socket(YAHOO_FINANCE_WEBSOCKET_URL);
+    subscribe_to_tickers(&mut socket);
+    let mut stream = Streamer::new(socket, handle_message);
+    stream.listen();
 }
--- src/streamer.rs
+++ src/streamer.rs
@@ -1,8 +1,38 @@
-use std::io::{Read, Write};
-
 use tungstenite::Message;
 
-pub struct Streamer<T: Read + Write> {
-    pub stream: T,
-    pub transformer: Box<dyn Fn(Message)>,
+pub type TungsteniteSocket =
+    tungstenite::WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>;
+
+pub struct Streamer {
+    pub socket: TungsteniteSocket,
+    pub on_message: fn(&mut TungsteniteSocket, Message, &mut [u8; 2048]),
+    buffer: [u8; 2048],
 }
+
+impl Streamer {
+    pub fn new(
+        socket: TungsteniteSocket,
+        on_message: fn(&mut TungsteniteSocket, Message, &mut [u8; 2048]),
+    ) -> Self {
+        Self {
+            socket,
+            on_message,
+            buffer: [0; 2048],
+        }
+    }
+
+    pub fn listen(&mut self) {
+        loop {
+            let msg = self.socket.read().unwrap();
+            (self.on_message)(&mut self.socket, msg, &mut self.buffer);
+        }
+    }
+}
+
+//
+// TODO:
+// refactor with
+// https://docs.rs/fastwebsockets/latest/fastwebsockets/
+// also... get binance stream as well!
+// do some comparisons?
+//
